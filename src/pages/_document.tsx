000d35d9c75d3af7e879b4009fe58c56a485e7b6
tjuandev <tpydeveloper@gmail.com>
2022-08-21 17:26:56 -0300

    feat: Improving _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,30 +12,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="description"
-            content="Este aqui é o WonderTodo, seu melhor gerenciador de tarefas."
-          />
-          <meta
-            name="keywords"
-            content="todo, tasks, tarefa, realtime, colaborativo"
-          />
-          <meta name="copyright" content="© Thiago Juan" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="apple-touch-icon" href="/favicon.png" />
-          <link rel="icon" href="/favicon.png" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
