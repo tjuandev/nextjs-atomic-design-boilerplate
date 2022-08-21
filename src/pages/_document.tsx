568e6c4e1058092e0aad0988ec131769ec1d687d
tjuandev <tpydeveloper@gmail.com>
2022-08-21 17:22:00 -0300

    feat: Adding and configuring styled components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,72 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
+import { Fragment } from 'react'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="description"
+            content="Este aqui é o WonderTodo, seu melhor gerenciador de tarefas."
+          />
+          <meta
+            name="keywords"
+            content="todo, tasks, tarefa, realtime, colaborativo"
+          />
+          <meta name="copyright" content="© Thiago Juan" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="apple-touch-icon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <Fragment key={1}>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>
+        ]
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
