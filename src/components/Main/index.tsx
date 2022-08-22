cdc173099bc855c7ff761e1c3eae77615b2369ea
tjuandev <tpydeveloper@gmail.com>
2022-08-22 19:07:58 -0300

    feat: Creating and styling Main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles'
+
+type MainProps = {
+  title?: string
+  description?: string
+}
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}: MainProps) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+)
+
+export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Main from 'components/Main'
+
 export default function Home() {
-  return <div>teste</div>
+  return <Main />
 }
