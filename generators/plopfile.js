694c0375c8e7f8bfd908c4e272559d705d5ed437
tjuandev <tpydeveloper@gmail.com>
2022-09-30 16:49:00 -0300

    feat: Making PlopFIle able to create atomic design layers

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,6 +1,26 @@
-module.exports = function (/** @type {import('plop').NodePlopAPI} */ plop) {
-  const componentsBasePath = '../src/components/{{pascalCase name}}'
+const atomicDesignLayers = [
+  {
+    name: 'Atom',
+    value: 'atoms'
+  },
+  {
+    name: 'Molecule',
+    value: 'molecules'
+  },
+  {
+    name: 'Organism',
+    value: 'organism'
+  },
+  {
+    name: 'Template',
+    value: 'template'
+  }
+]
 
+const componentsBasePath =
+  '../src/components/{{component_type}}/{{pascalCase name}}'
+
+module.exports = function (/** @type {import('plop').NodePlopAPI} */ plop) {
   plop.setGenerator('components', {
     description: 'application component logic',
     prompts: [
@@ -8,13 +28,24 @@ module.exports = function (/** @type {import('plop').NodePlopAPI} */ plop) {
         type: 'input',
         name: 'name',
         message: 'component name please'
+      },
+      {
+        type: 'list',
+        name: 'component_type',
+        choices: atomicDesignLayers
       }
     ],
     actions: [
       {
         type: 'add',
+        path: `${componentsBasePath}/index.ts`,
+        skipIfExists: true
+      },
+      {
+        type: 'add',
         path: `${componentsBasePath}/__tests__/{{pascalCase name}}.tsx`,
-        templateFile: 'templates/components/__tests__/index.tsx.hbs'
+        templateFile: 'templates/components/__tests__/index.tsx.hbs',
+        force: true
       },
       {
         type: 'add',
