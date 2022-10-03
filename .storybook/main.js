16034e5e32da6527a4587dc05461a538014a83fe
tjuandev <tpydeveloper@gmail.com>
2022-10-03 18:22:44 -0300

    feat: configuring absolute paths to storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.mdx",
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../src/components/**/stories.mdx',
+    '../src/components/**/stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
   }
 }
