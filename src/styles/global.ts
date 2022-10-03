baa3b8a8a2ce8f4d44c66d8d8bf054b1d4820fd6
tjuandev <tpydeveloper@gmail.com>
2022-10-03 17:39:23 -0300

    feat: Adding antialising featurefor mac

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,9 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   body,
